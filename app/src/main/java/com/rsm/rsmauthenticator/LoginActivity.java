694cf60362da8e4ef87b93153519da6c3a09fd0a
jimboharrison <james.jharrison@hotmail.co.uk>
2016-01-22 19:05:57 +0000

    Started to call api on login activity

--- app/src/main/java/com/rsm/rsmauthenticator/LoginActivity.java
+++ app/src/main/java/com/rsm/rsmauthenticator/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.rsm.rsmauthenticator;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.loopj.android.http.*;
+import org.json.*;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by James on 08/01/2016.
@@ -14,6 +22,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     Button btLogin;
     EditText etEmail, etPassword;
+    TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -31,8 +40,46 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
             //if login button is clicked
             case R.id.btLogin:
-                startActivity(new Intent(this, MainActivity.class));
+                RequestParams params = new RequestParams();
+                params.add("email", etEmail.toString());
+                params.add("password", etPassword.toString());
+                params.add("password", telephonyManager.getDeviceId());
+                HttpClient.get("Account", params, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the response is JSONObject instead of expected JSONArray
+                        //http://loopj.com/android-async-http/
+                        goToMainActivity();
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                        // Pull out the first event on the public timeline
+                        JSONObject firstEvent = null;
+                        try {
+                            firstEvent = (JSONObject) timeline.get(0);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        String tweetText = null;
+                        try {
+                            tweetText = firstEvent.getString("text");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Do something with the response
+                        System.out.println(tweetText);
+                    }
+
+
+                });
+
                 break;
         }
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }

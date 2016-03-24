4a6790c15cee9fb3a632aa5d6af4e1430e8ede4c
jimboharrison <james.jharrison@hotmail.co.uk>
2016-03-24 18:35:22 +0000

    Now renders different notification depending on users settings

--- app/src/main/java/com/rsm/rsmauthenticator/AccessResponseActivity.java
+++ app/src/main/java/com/rsm/rsmauthenticator/AccessResponseActivity.java
@@ -9,36 +9,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class AccessRequestActivity extends AppCompatActivity {
+public class AccessResponseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_access_request);
+        setContentView(R.layout.activity_access_response);
 
         Intent myIntent = getIntent(); // gets the previously created intent
         String otp = myIntent.getStringExtra("otp"); // will return otp
         String requestTime = myIntent.getStringExtra("requestTime");
         String username = myIntent.getStringExtra("username");
         String appName = myIntent.getStringExtra("appName");
-
+        String requestId = myIntent.getStringExtra("requestId");
 
         SimpleDateFormat format = new SimpleDateFormat("MMM EEE dd HH:mm:ss zzz yyyy");
 
-        try {
+        try{
             Date parseDate = format.parse(requestTime);
-        } catch (ParseException e) {
+        }catch (ParseException e) {
             e.printStackTrace();
         }
 
-        TextView tvUsername = (TextView)findViewById(R.id.requestusername);
-        TextView tvApp = (TextView)findViewById(R.id.requestapplication);
-        TextView tvDate = (TextView)findViewById(R.id.requestdate);
-        TextView tvOtp = (TextView) findViewById(R.id.requestotp);
+        TextView tvUsername = (TextView)findViewById(R.id.responseRequestUsername);
+        TextView tvApp = (TextView)findViewById(R.id.responseRequesApplication);
+        TextView tvDate = (TextView)findViewById(R.id.responseRequestDate);
+        TextView tvOtp = (TextView)findViewById(R.id.responseRequesOtp);
 
         tvUsername.setText(username);
         tvApp.setText(appName);
--- app/src/main/java/com/rsm/rsmauthenticator/GcmMessageHandler.java
+++ app/src/main/java/com/rsm/rsmauthenticator/GcmMessageHandler.java
@@ -20,27 +20,48 @@ public class GcmMessageHandler extends GcmListenerService {
         String time = data.getString("time");
         String userFullName = data.getString("username");
         String appName = data.getString("app");
+        String method = data.getString("method");
+        String requestId = data.getString("id");
+
         String notificationTitle = "RSM Authentication";
-        createNotification(from, otp, time, userFullName, appName, notificationTitle);
+        createNotification(from, otp, time, userFullName, appName, notificationTitle, method, requestId);
     }
 
     // this method will create a notification from the message recieved
-    private void createNotification(String from, String body, String time, String userName, String appName, String notificationTitle) {
+    private void createNotification(String from, String body, String time, String userName, String appName, String notificationTitle, String method, String requestId) {
         Context context = getBaseContext();
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.mipmap.ic_launcher).setContentTitle(notificationTitle)
                 .setContentText("Access Request for " + appName);
 
+        CreateIntent(mBuilder, body, time, userName, appName, method, requestId);
+
+        Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE); vibrator.vibrate(2000);
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(notification_Id, mBuilder.build());
+    }
+
+    private void CreateIntent(NotificationCompat.Builder mBuilder, String body, String time, String userName, String appName, String method, String requestId) {
         mBuilder.setTicker("RSM Authentication Required");
         mBuilder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
         mBuilder.setOnlyAlertOnce(true);
         //need to get vibrations working
 
-        Intent resultIntent = new Intent(this, AccessRequestActivity.class);
+        Intent resultIntent;
+
+        if(method.equals("OneTimePasscode")){
+            resultIntent = new Intent(this, AccessRequestActivity.class);
+        }else if (method.equals("PushNotification")){
+            resultIntent = new Intent(this, AccessResponseActivity.class);
+        }else resultIntent = new Intent(this, MainActivity.class);//if no method is set then just send the user to the main activity
+
         resultIntent.putExtra("otp", body);
         resultIntent.putExtra("requestTime", time);
         resultIntent.putExtra("username", userName);
         resultIntent.putExtra("appName", appName);
+        resultIntent.putExtra("requestId", requestId);
 
         PendingIntent resultPendingIntent =
                 PendingIntent.getActivity(
@@ -51,11 +72,5 @@ public class GcmMessageHandler extends GcmListenerService {
                 );
 
         mBuilder.setContentIntent(resultPendingIntent);
-
-        Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE); vibrator.vibrate(2000);
-        NotificationManager mNotificationManager = (NotificationManager) context
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-
-        mNotificationManager.notify(notification_Id, mBuilder.build());
     }
 }
